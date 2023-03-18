3a706bf5e206c241bc635a5488ae8807f9df2189
Florencio Britez <florenciobritez49@gmail.com>
2023-03-17 23:44:44 -0300

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <article>
+        <h1>First Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, nesciunt laboriosam dicta quos ducimus commodi, architecto nihil autem rerum
+           voluptatum exercitationem possimus? Delectus vel dignissimos fugit accusantium dolorem dolorum distinctio!
+        </p>
+    </article>
+
+    <article>
+        <h1>Second Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, nesciunt laboriosam dicta quos ducimus commodi, architecto nihil autem rerum
+           voluptatum exercitationem possimus? Delectus vel dignissimos fugit accusantium dolorem dolorum distinctio!
+        </p>
+    </article>
+</body>
